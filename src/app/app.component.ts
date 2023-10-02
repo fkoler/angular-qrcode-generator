01e682c50e0e2d5e4cacc6e54d43c96b06afb8e2
ferenc_koler <ferenckoler@outlook.com>
2023-10-02 19:19:38 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeValue } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularqrgen';
+  title = 'AngularQRCodeGenerator';
+
+  qrdata = '';
+  qrCodeDownloadLink: SafeValue = '';
+
+  onChange(url: any) {
+    this.qrCodeDownloadLink = url;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    QRCodeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
